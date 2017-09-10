1549a48a25048996614f5f2e79762074a6c80623
Amit Kriplani <amitkriplani@ymail.com>
2017-09-11 01:38:19 +0530

    added nginx, php-fpm to startup

--- vmss/lampserver.sh
+++ vmss/lampserver.sh
@@ -11,6 +11,8 @@ yum install -y ./epel-release-latest-7.noarch.rpm
 yum install -y nginx
 curl https://raw.githubusercontent.com/amitkriplani/azure-scripts/master/vmss/nginx-default.conf > /etc/nginx/conf.d/default.conf
 curl https://raw.githubusercontent.com/amitkriplani/azure-scripts/master/vmss/nginx.conf > /etc/nginx/nginx.conf
+
+systemctl enable nginx
 service nginx start
 
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
@@ -22,4 +24,6 @@ yum install -y php56w-fpm
 curl https://raw.githubusercontent.com/amitkriplani/azure-scripts/master/vmss/php-fpm-www.conf > /etc/php-fpm.d/www.conf
 mkdir /var/www/default
 curl https://raw.githubusercontent.com/amitkriplani/azure-scripts/master/vmss/default-index.php > /var/www/default/index.php
+
+systemctl enable php-fpm
 service php-fpm start
